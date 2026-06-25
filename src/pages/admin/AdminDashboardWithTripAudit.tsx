17c711639be91aea9e6bf4f9366719d65668e991
dawaapharmacy29-create <dawaapharmacy29@gmail.com>
2026-06-25 15:41:17 +0300

    Add admin dashboard trip audit wrapper

---
+++ src/pages/admin/AdminDashboardWithTripAudit.tsx
@@ -0,0 +1,6 @@
+import TripFraudWatch from '../../components/TripFraudWatch'
+import AdminDashboard from './AdminDashboard'
+
+export default function AdminDashboardWithTripAudit() {
+  return <><TripFraudWatch /><AdminDashboard /></>
+}
