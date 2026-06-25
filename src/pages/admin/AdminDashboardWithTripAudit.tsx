b42a9d82de97a88d3e4fbaf5daea2302ae7ee7f6
dawaapharmacy29-create <dawaapharmacy29@gmail.com>
2026-06-25 19:34:59 +0300

    Show cycle history on dashboard

--- src/pages/admin/AdminDashboardWithTripAudit.tsx
+++ src/pages/admin/AdminDashboardWithTripAudit.tsx
@@ -1,11 +1,15 @@
 import AdminDashboard from './AdminDashboard'
 import DashboardTripCustomerInsights from '../../components/DashboardTripCustomerInsights'
+import CycleArchiveOverview from '../../components/CycleArchiveOverview'
 
 export default function AdminDashboardWithTripAudit() {
   return (
-    <div className="space-y-5" dir="rtl">
-      <AdminDashboard />
-      <DashboardTripCustomerInsights />
-    </div>
+    <>
+      <div className="space-y-5" dir="rtl">
+        <AdminDashboard />
+        <DashboardTripCustomerInsights />
+        <CycleArchiveOverview />
+      </div>
+    </>
   )
 }
