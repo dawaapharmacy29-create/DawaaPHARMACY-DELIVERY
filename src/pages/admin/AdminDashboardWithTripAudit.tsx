2f669c117f206d4ed39730813f84cbbd9f579a29
dawaapharmacy29-create <dawaapharmacy29@gmail.com>
2026-07-11 07:53:23 +0300

    Use reliable paginated admin dashboards

--- src/pages/admin/AdminDashboardWithTripAudit.tsx
+++ src/pages/admin/AdminDashboardWithTripAudit.tsx
@@ -1,15 +1,15 @@
-import AdminDashboard from './AdminDashboard'
+import AdminDashboardReliable from './AdminDashboardReliable'
 import DashboardTripCustomerInsights from '../../components/DashboardTripCustomerInsights'
 import CycleArchiveOverview from '../../components/CycleArchiveOverview'
-import DashboardGrowthPanel from '../../components/DashboardGrowthPanel'
+import DashboardGrowthPanelReliable from '../../components/DashboardGrowthPanelReliable'
 
 export default function AdminDashboardWithTripAudit() {
   return (
     <>
       <div className="space-y-5" dir="rtl">
-        <AdminDashboard />
+        <AdminDashboardReliable />
         <DashboardTripCustomerInsights />
-        <DashboardGrowthPanel />
+        <DashboardGrowthPanelReliable />
         <CycleArchiveOverview />
       </div>
     </>
