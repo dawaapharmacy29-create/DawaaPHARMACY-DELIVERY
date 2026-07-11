5ff21f2ff36bf0ebb3140d976da94ba1a6e9c562
dawaapharmacy29-create <dawaapharmacy29@gmail.com>
2026-07-11 08:06:38 +0300

    Restore full dashboard feature set with reliable data panels

--- src/pages/admin/AdminDashboardWithTripAudit.tsx
+++ src/pages/admin/AdminDashboardWithTripAudit.tsx
@@ -2,16 +2,24 @@ import AdminDashboardReliable from './AdminDashboardReliable'
 import DashboardTripCustomerInsights from '../../components/DashboardTripCustomerInsights'
 import CycleArchiveOverview from '../../components/CycleArchiveOverview'
 import DashboardGrowthPanelReliable from '../../components/DashboardGrowthPanelReliable'
+import RiderAppVersionStatus from '../../components/RiderAppVersionStatus'
+import RiderOperationsHealth from '../../components/RiderOperationsHealth'
+import LiveRiderLeaderboard from '../../components/LiveRiderLeaderboard'
 
 export default function AdminDashboardWithTripAudit() {
   return (
-    <>
-      <div className="space-y-5" dir="rtl">
-        <AdminDashboardReliable />
-        <DashboardTripCustomerInsights />
-        <DashboardGrowthPanelReliable />
-        <CycleArchiveOverview />
-      </div>
-    </>
+    <div className="space-y-5" dir="rtl">
+      <AdminDashboardReliable />
+
+      <section className="grid gap-5 xl:grid-cols-2">
+        <RiderAppVersionStatus />
+        <LiveRiderLeaderboard />
+      </section>
+
+      <RiderOperationsHealth />
+      <DashboardTripCustomerInsights />
+      <DashboardGrowthPanelReliable />
+      <CycleArchiveOverview />
+    </div>
   )
 }
